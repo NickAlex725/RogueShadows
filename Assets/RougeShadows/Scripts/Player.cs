c46b4ac2964214492c459a1ad18acd8cd881bae4
Nick Hanson <nickalex725@gmail.com>
2024-03-08 21:46:36 -0600

    Character Controller set up
    - movement
    - jumping
    - sprinting
    - gravity

--- Assets/RougeShadows/Scripts/Player.cs
+++ Assets/RougeShadows/Scripts/Player.cs
@@ -3,48 +3,102 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private GameObject _art;
+    [Header("Player Info")]
+    [SerializeField] private int _moveSpeed;
+    [SerializeField] private int _sprintSpeed;
+    [SerializeField] private int _jumpPower;
     [SerializeField] private int _dashStrength;
+    [SerializeField] private float _gravityMultiplier = 3.0f;
 
-    private Rigidbody _rb;
+    [Header("References")]
+    [SerializeField] private GameObject _art;
+
+    private CharacterController _characterController;
+    private Vector2 _move;
+    private int _currentMoveSpeed;
+    private Vector3 _direction;
+    private float _currnetVelocity;
+    private float _velocity;
+    private float _gravity = -9.81f;
     private bool _canDash = true; //set to true for testing purposes
     private bool _isDashing;
 
+
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
+        _characterController = GetComponent<CharacterController>();
+        _currentMoveSpeed = _moveSpeed;
     }
 
-    public void OnShadowDash(InputValue value)
+    private void Update()
+    {
+        ApplyGravity();
+        ApplyRotation();
+        ApplyMovement();
+    }
+
+    public void OnShadowDash(InputAction.CallbackContext context)
     {
         Debug.Log("Dashing");
         if(_canDash)
         {
             _isDashing = true;
-            _rb.AddForce(Vector3.forward * _dashStrength, ForceMode.Force);
         }
     }
 
-    public void OnMove(InputValue value)
+    public void Move(InputAction.CallbackContext context)
     {
-        Debug.Log("moving");
+        _move = context.ReadValue<Vector2>();
+        _direction = new Vector3(_move.x, 0.0f, _move.y);
     }
 
-    public void OnLook(InputValue value)
+    public void Jump(InputAction.CallbackContext context)
     {
-        Debug.Log("Looking");
+        if (!context.started) return;
+        if (!_characterController.isGrounded) return;
+
+        _velocity += _jumpPower;
     }
 
-    public void OnJump(InputValue value)
+    public void Sprint(InputAction.CallbackContext context)
     {
-        Debug.Log("Jumping");
+        if (context.started)
+        {
+            _currentMoveSpeed = _sprintSpeed;
+        }
+        else if(context.canceled)
+        {
+            _currentMoveSpeed = _moveSpeed;
+        }
     }
 
-    public void OnSprint(InputValue value)
+    private void ApplyRotation()
     {
-        Debug.Log("Sprinting");
+        if (_move.sqrMagnitude == 0) return;
+
+        var targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg;
+        var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currnetVelocity, 0.05f);
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+    }
+
+    private void ApplyMovement()
+    {
+        _characterController.Move(_direction * _currentMoveSpeed * Time.deltaTime);
+    }
+
+    private void ApplyGravity()
+    {
+        if(_characterController.isGrounded && _velocity < 0.0f)
+        {
+            _velocity = -1.0f;
+        }
+        else
+        {
+            _velocity += _gravity * _gravityMultiplier * Time.deltaTime;
+        }
+        _direction.y = _velocity;
     }
 }
